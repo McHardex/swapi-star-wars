ab5421fd0c8c649bc6a8c2c9db8160028736ee8e
mchardex <bukunmiadebisi@gmail.com>
2019-09-20 04:25:55 +0100

    ft(header): create reusable headers

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
+import Header from '../shared/Header';
 
 const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header transition />
     </div>
   );
 };
