4a23ee5f65a56e6c191222fa7ebf23c71f64de2c
mchardex <bukunmiadebisi@gmail.com>
2019-10-09 13:02:01 +0100

    ft(ui) - style profile page
    - create user profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import Profile from './components/Profile';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Route exact path="/" component={Home} />
+      <Route path="/profile" component={Profile} />
     </div>
   );
 };
